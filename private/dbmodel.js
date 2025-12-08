3459c556ab19d40cfc9adb69d67bc347d9211cae
NDagger <yartsiz@gmail.com>
2025-12-08 10:49:51 +0100

    README.md update, root .env file removed, compose.yaml uses env file from public/.env

--- private/dbmodel.js
+++ private/dbmodel.js
@@ -1,14 +1,14 @@
 const { Pool } = require('pg');
 
-const { DB_NAME, POSTGRES_USER, POSTGRES_PASSWORD, DB_HOST, DB_DEV_HOST, DB_PORT } = process.env;
+const { POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, DB_HOST, DB_DEV_HOST, DB_PORT } = process.env;
 
-console.log(DB_NAME, POSTGRES_USER, POSTGRES_PASSWORD, DB_HOST, DB_PORT)
+console.log(POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, DB_HOST, DB_PORT)
 const pool = new Pool({
   host: DB_HOST,       // имя сервиса в docker-compose
   port: DB_PORT,       // внутренний порт PostgreSQL
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
-  database: DB_NAME,
+  database: POSTGRES_DB,
 });
 
 module.exports = { query: (text, params) => pool.query(text, params) };
