e60f71c11a2790b604d7daccb345c1de7714d195
NDagger <yartsiz@gmail.com>
2025-12-12 17:36:49 +0100

    cart.js removed

--- private/models/cart.js
+++
@@ -1,8 +0,0 @@
-const User = require('./user');
-const { query } = require('../dbmodel');
-
-const { getBase64Image } = require('../models/images');
-
-// Cart contains ONLY sized items from item_sizes table.
-
-// module.exports = { addCartItem, getCartItems, removeCartItem };
